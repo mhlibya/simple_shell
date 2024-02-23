7151c5e24a2abca30f8327773e6f9fdc1fc8bdbd
mhlibya <mhlibya728@gmail.com>
2024-02-23 05:27:15 -0800

    shell

---
+++ shell.h
@@ -0,0 +1,40 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*---LIBRARIES---*/
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
+
+/*---PROTOTYPES---*/
+/* main.c */
+void shell_interactive(void);
+void shell_no_interactive(void);
+/*---Macros---*/
+#define TOK_DELIM " \t\r\n\a\""
+extern char **environ;
+/*---PROTOTYPES---*/
+/* main.c */
+void shell_interactive(void);
+void shell_no_interactive(void);
+/* shell_interactive.c */
+char *read_line(void);
+char **split_line(char *line);
+int execute_args(char **args);
+/* execute_args */
+int new_process(char **args);
+/* shell_no_interactive */
+char *read_stream(void);
+/*---Builtin func---*/
+int own_cd(char **args);
+int own_exit(char **args);
+int own_env(char **args);
+int own_help(char **args);
+#endif
